19a30ef652f7adbfea9b0a8c85c630fa1ec375cf
levanduy <levanduyavatar@gmail.com>
2021-10-29 18:43:56 +0700

    solution 2 for remove duplicates

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -6,6 +6,7 @@ class RemoveDuplicates {
         }
     } 
 
+    //solution 1
     public static int removeDuplicates(int[] nums) {
 
         int k = nums.length;
@@ -20,11 +21,31 @@ class RemoveDuplicates {
 
         return k;
     }
+
+    //solution 2
+    public static int removeDuplicatesElement(int[] nums) {
+
+        int size = nums.length;
+        int k = 0;
+
+        if (size < 2) {
+            return size;
+        }
+
+        for (int i = 1; i < size; i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[++k] = nums[i];
+            }
+        }
+
+        return k+1;
+    }
     
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2, 2, 2, 3, 4, 4, 5, 5, 5, 5}; 
+        int[] nums = {1, 1, 1, 2, 2, 2, 3, 4, 4, 5, 5, 5, 5}; 
         
-        int k = removeDuplicates(nums);
+        // int k = removeDuplicates(nums);
+        int k = removeDuplicatesElement(nums);
 
         for (int i = 0; i < k; i++) {
             System.out.println(nums[i]);
