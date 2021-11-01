47dd7280cf98fb23aaf35b64aaac925c79147ab2
levanduy <levanduyavatar@gmail.com>
2021-11-01 22:35:46 +0700

    solution 2 for valid parentheses

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class ValidParentheses {
 
     //solution 1
@@ -38,8 +40,29 @@ class ValidParentheses {
         }
     }
 
+    //solution 2
+    public static boolean isValidParentheses(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' && !stack.empty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && !stack.empty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && !stack.empty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.empty();
+    }
+
     public static void main(String[] args) {
         String s = "([)]";
-        System.out.println(isValid(s));
+        System.out.println(isValidParentheses(s));
     }
 }
