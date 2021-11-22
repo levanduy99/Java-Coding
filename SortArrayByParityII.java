139a64e6f3d2b8ebab9f23e264658745339f7456
levanduy <levanduyavatar@gmail.com>
2021-11-22 20:48:48 +0700

    sort array by parity II

--- SortArrayByParityII.java
+++ SortArrayByParityII.java
@@ -21,21 +21,72 @@ public class SortArrayByParityII {
     }
 
     // solution 2
-    public static int[] sortArrayByParityII(int[] nums) {
+    public static int[] sortArrayByParityII2(int[] nums) {
 
-        int l = 0;
-        int r = nums.length - 1;
+        int indexEven = 0;
+        int indexOdd = nums.length - 1;
 
-        while (l < r) {
+        while (indexEven < nums.length - 1 && indexOdd > 0) {
+
+            if (nums[indexEven] % 2 == 0) {
+                indexEven += 2;
+            } else if (nums[indexOdd] % 2 == 0) {
+                int temp = nums[indexEven];
+                nums[indexEven] = nums[indexOdd];
+                nums[indexOdd] = temp;
+
+                indexEven += 2;
+                indexOdd -= 2;
+            }
+            
+            if (indexEven < nums.length - 1 && indexOdd > 0) {
+                if (nums[indexOdd] % 2 != 0) {
+                    indexOdd -= 2;
+                } else if (nums[indexEven] % 2 != 0) {
+                    int temp = nums[indexEven];
+                    nums[indexEven] = nums[indexOdd];
+                    nums[indexOdd] = temp;
+
+                    indexEven += 2;
+                    indexOdd -= 2;
+                }
+            }
 
         }
 
         return nums;
     }
 
+    // solution 3
+    public static int[] sortArrayByParityII(int[] nums) {
+
+        int indexEven = 0;
+        int indexOdd = 1;
+        int size = nums.length;
+
+        while (indexEven < size && indexOdd < size) {
+
+            while (indexEven < size && nums[indexEven] % 2 == 0) {
+                indexEven += 2;
+            }
+
+            while (indexOdd < size && nums[indexOdd] % 2 != 0) {
+                indexOdd += 2;
+            }
+
+            if (indexEven < size && indexOdd < size) {
+                int temp = nums[indexEven];
+                nums[indexEven] = nums[indexOdd];
+                nums[indexOdd] = temp;
+            }
+        }
+
+        return nums;
+    }    
+
     public static void main(String[] args) {
-        int[] nums = { 2, 3, 1, 1, 4, 0, 0, 4, 3, 3 };
-        for (int num : sortArrayByParityII1(nums)) {
+        int[] nums = {4, 2, 5, 7};
+        for (int num : sortArrayByParityII(nums)) {
             System.out.print(num + " ");
         }
     }
