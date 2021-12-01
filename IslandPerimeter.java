522fa3f425befe36186fb6de00b71fc48d94844a
levanduy <levanduyavatar@gmail.com>
2021-12-01 21:10:26 +0700

    island perimeter

---
+++ IslandPerimeter.java
@@ -0,0 +1,34 @@
+public class IslandPerimeter {
+    
+    static int islandPerimeter(int[][] grid) {
+
+        int islandPerimeter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    islandPerimeter += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        islandPerimeter -= 2;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        islandPerimeter -= 2;
+                    }
+                } 
+            }
+        }
+
+        return islandPerimeter;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] grid = {
+            {0,1,0,0},
+            {1,1,1,0},
+            {0,1,0,0},
+            {1,1,0,0}
+        };
+        System.out.println(islandPerimeter(grid));
+    }
+}
--- README.md
+++ README.md
@@ -172,6 +172,14 @@ The **area** of an island is the number of cells with a value `1` in the island.
 
 Return *the maximum **area** of an island in `grid`*. If there is no island, return `0`.
 
+### 28. Island Perimeter
+
+You are given `row x col` `grid` representing a map where `grid[i][j] = 1` represents land and `grid[i][j] = 0` represents water.
+
+Grid cells are connected **horizontally/vertically** (not diagonally). The `grid` is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around island. One cell is a square with side length = 1. the grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
 ## Strings
 
 ### 1. Reverse String 
